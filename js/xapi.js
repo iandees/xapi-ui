490d0bcbdb0259f38a7f054c2bafc04997297e90
Serge Wroclawski <serge@wroclawski.org>
2011-01-02 14:56:57 -0500

    Clean start

--- js/xapi.js
+++ js/xapi.js
@@ -2,48 +2,14 @@ $(document).ready(function() {
 
   // Set up the map
   var map = new OpenLayers.Map('bboxmap');
-  // var osm = new OpenLayers.Layer.OSM.Mapnik("Mapnik");
-  // var osm = new OpenLayers.Layer.OSM;
-//  var map = new OpenLayers.Map ("bboxmap", {
-//    controls: [
-//      new OpenLayers.Control.Navigation()
-//    ],
-//    maxExtent: new OpenLayers.Bounds(-20037508.34,-20037508.34,
-//                                     20037508.34,20037508.34),
-//    numZoomLevels: 20,
-//    maxResolution: 156543.0339,
-//    displayProjection: new OpenLayers.Projection("EPSG:4326"),
-//    units: 'm',
-//    projection: new OpenLayers.Projection("EPSG:900913")
-//  });
   var osm = new OpenLayers.Layer.OSM.Mapnik("Mapnik", {
-    displayOutsideMaxExtent: true,
-    wrapDateLine: true,
     attribution: ''
   });
 
   map.addLayer(osm);
-  //map.zoomToMaxExtent();
   map.zoomTo(1);
 
-  // Set up the function for the control box
-  var control = new OpenLayers.Control();
-  OpenLayers.Util.extend(control, {
-    draw: function () {
-      // this Handler.Box will intercept the shift-mousedown
-      // before Control.MouseDefault gets to see it
-      this.box = new OpenLayers.Handler.Box( control,
-                                             {"done": this.notice},
-                                             {keyMask: OpenLayers.Handler.MOD_SHIFT});
-      this.box.activate();
-    },
-    
-    notice: function (bounds) {
-      OpenLayers.Console.userError(bounds);
-    }
-  });
-  map.addControl(control);
-
+  
   // Set up some other basics
   var baseurl = "http://xapi-server"
 
@@ -64,7 +30,6 @@ $(document).ready(function() {
   
   var update_results = function() {
     var results = baseurl + '/' ;
-    // Foo
     if ($('#searchbytag').is(':checked')) {
       results = results + tagsearch();
       if ($('#searchbybbox').is(':checked')) {
